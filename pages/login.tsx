3098cc2aee12ac39398eac806b64d3cf414378ec
bluewave41 <megaman22342@live.ca>
2022-04-19 17:56:14 -0700

    Added basic page structure

---
+++ pages/login.tsx
@@ -0,0 +1,42 @@
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import axios from 'axios';
+
+const Login = (props) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onChange = (e) => {
+        switch(e.target.name) {
+            case 'username':
+                setUsername(e.target.value);
+                break;
+            case 'password':
+                setPassword(e.target.value);
+                break;
+        }
+    }
+
+    const onSubmit = async (e) => {
+        let response;
+        try {
+            response = await axios.post('/api/auth/login', { username: username, password: password });
+        }
+        catch(e) {
+
+        }
+    }
+
+    return (
+        <Box sx={{ width: '30%', display: 'flex', flexDirection: 'column' }}>
+            <h1>Login</h1>
+            <TextField onChange={onChange} />
+            <TextField onChange={onChange} />
+            <Button variant='contained' onClick={onSubmit}>Submit</Button>
+        </Box>
+    )
+}
+
+export default Login;
